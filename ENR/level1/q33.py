3b0f4d7a6d7ea7ef1cb4a205724c3f519d750c2c
fenil <fenilp2046@gmail.com>
2024-09-20 23:54:15 +0530

    got it

--- ENR/level1/q33.py
+++ ENR/level1/q33.py
@@ -5,8 +5,8 @@ for i in range(num):
     l1.append(push)
 print(l1)
 # Doubt ???
-# rem=input("Enter to remove : ")
-# for i in range(len(l1)):
-#     if rem==l1[i]:
-#         l1.remove(l1[i])
-# print(l1)
+rem=input("Enter to remove : ")
+for i in l1[:]:
+    if rem==i:
+        l1.remove(i)
+print(l1)
