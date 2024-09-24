6ec6f8acf05e63bbb94334f7b7cf76990c814f1c
fenil <fenilp2046@gmail.com>
2024-09-24 16:37:11 +0530

    eje

---
+++ ENR/level3/q5.py
@@ -0,0 +1,13 @@
+num=int(input("enter till you want to print fabonacci "))
+p1=1
+p2=0
+print(1)
+even=0
+for i in range(num):
+    sum=p1+p2
+    print(sum,end="")
+    p2=p1
+    p1=sum
+    if sum%2==0:
+        even+=sum
+print(f"the sum of even numbers in the series is {even}")
